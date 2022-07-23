c8f6e1aa23d55e654a9f4032997b32f2be441d87
Yavuz Selim Tozlu <0yavuz0@gmail.com>
2022-07-23 17:24:54 -0400

    Add texture data and flip

--- model.cpp
+++ model.cpp
@@ -5,49 +5,116 @@
 #include <vector>
 #include "model.hpp"
 
-Model::Model(const char *filename) : verts_(), faces_() {
-    std::ifstream in;
-    in.open (filename, std::ifstream::in);
-    if (in.fail()) return;
-    std::string line;
-    while (!in.eof()) {
-        std::getline(in, line);
-        std::istringstream iss(line.c_str());
-        char trash;
-        if (!line.compare(0, 2, "v ")) {
-            iss >> trash;
-            Vec3f v;
-            for (int i=0;i<3;i++) iss >> v.raw[i];
-            verts_.push_back(v);
-        } else if (!line.compare(0, 2, "f ")) {
-            std::vector<int> f;
-            int itrash, idx;
-            iss >> trash;
-            while (iss >> idx >> trash >> itrash >> trash >> itrash) {
-                idx--; // in wavefront obj all indices start at 1, not zero
-                f.push_back(idx);
-            }
-            faces_.push_back(f);
+Model::Model(const char *filename, bool flip_vertical) : verts_(), faces_(), uv_(),
+                                                         textureIdx_()
+{
+  std::ifstream in;
+  in.open(filename, std::ifstream::in);
+  if (in.fail())
+    return;
+  std::string line;
+  if (flip_vertical)
+  {
+    while (!in.eof())
+    {
+      std::getline(in, line);
+      std::istringstream iss(line.c_str());
+      char trash;
+      if (!line.compare(0, 2, "v "))
+      {
+        iss >> trash;
+        Vec3f v;
+
+        float temp;
+        for (int i = 0; i < 3; i++)
+        {
+          iss >> temp;
+          if (i == 1)
+            temp = -temp;
+          v.raw[i] = temp;
         }
+        verts_.push_back(v);
+      }
+      else if (!line.compare(0, 2, "f "))
+      {
+        std::vector<int> f;
+        std::vector<int> t;
+        int itrash, idx, tidx;;
+        iss >> trash;
+        while (iss >> idx >> trash >> tidx >> trash >> itrash)
+        {
+          idx--; // in wavefront obj all indices start at 1, not zero
+          tidx--;
+          f.push_back(idx);
+          t.push_back(tidx);
+        }
+        faces_.push_back(f);
+        textureIdx_.push_back(t);
+      }
+      else if (!line.compare(0, 3, "vt "))
+      {
+        iss >> trash >> trash;
+
+        float u,v;
+        iss >> u >> v;
+
+        uv_.push_back(Vec2f(u,v));
+      }
     }
-    std::cerr << "# v# " << verts_.size() << " f# "  << faces_.size() << std::endl;
+  }
+  else
+  {
+    while (!in.eof())
+    {
+      std::getline(in, line);
+      std::istringstream iss(line.c_str());
+      char trash;
+      if (!line.compare(0, 2, "v "))
+      {
+        iss >> trash;
+        Vec3f v;
+        float temp;
+        for (int i = 0; i < 3; i++)
+          iss >> v.raw[i];
+        verts_.push_back(v);
+      }
+      else if (!line.compare(0, 2, "f "))
+      {
+        std::vector<int> f;
+        int itrash, idx;
+        iss >> trash;
+        while (iss >> idx >> trash >> itrash >> trash >> itrash)
+        {
+          idx--; // in wavefront obj all indices start at 1, not zero
+          f.push_back(idx);
+        }
+        faces_.push_back(f);
+      }
+    }
+  }
+  std::cerr << "# v# " << verts_.size() << " f# " << faces_.size() << std::endl;
 }
 
-Model::~Model() {
+Model::~Model()
+{
 }
 
-int Model::nverts() {
-    return (int)verts_.size();
+int Model::nverts()
+{
+  return (int)verts_.size();
 }
 
-int Model::nfaces() {
-    return (int)faces_.size();
+int Model::nfaces()
+{
+  return (int)faces_.size();
 }
 
-std::vector<int> Model::face(int idx) {
-    return faces_[idx];
+std::vector<int> Model::face(int idx)
+{
+  return faces_[idx];
 }
 
-Vec3f Model::vert(int i) {
-    return verts_[i];
+Vec3f Model::vert(int i)
+{
+  return verts_[i];
 }
--- model.hpp
+++ model.hpp
@@ -8,13 +8,17 @@ class Model {
 private:
 	std::vector<Vec3f> verts_;
 	std::vector<std::vector<int> > faces_;
+	std::vector<Vec2f> uv_;
+	std::vector<std::vector<int>> textureIdx_;
 public:
-	Model(const char *filename);
+	Model(const char *filename, bool flip_vertical);
 	~Model();
 	int nverts();
 	int nfaces();
 	Vec3f vert(int i);
 	std::vector<int> face(int idx);
+	Vec2f uv(int i);
+	Vec3i textureIdx(int idx);
 };
 
 #endif //__MODEL_H__
