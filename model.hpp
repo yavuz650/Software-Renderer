4b6629b480d535e3e9109b8aca1a532eefd888b6
Yavuz Selim Tozlu <0yavuz0@gmail.com>
2022-08-07 22:05:32 -0400

    Stoppd flipping vertices

--- model.hpp
+++ model.hpp
@@ -11,14 +11,14 @@ private:
 	std::vector<Vec2f> uv_;
 	std::vector<std::vector<int>> textureIdx_;
 public:
-	Model(const char *filename, bool flip_vertical);
+	Model(const char *filename);
 	~Model();
 	int nverts();
 	int nfaces();
 	Vec3f vert(int i);
 	std::vector<int> face(int idx);
 	Vec2f uv(int i);
-	Vec3i textureIdx(int idx);
+	std::vector<int> textureIdx(int idx);
 };
 
 #endif //__MODEL_H__
