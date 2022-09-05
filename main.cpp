362be817dad1a1905ed0de765c2347ad15540877
Yavuz Selim Tozlu <0yavuz0@gmail.com>
2022-09-05 01:31:27 -0400

    Complete overhaul of the renderer.
    In the previous versions, triangles were drawn on the screen one by one, in a sequential manner. The renderer would completely finish drawing the current triangle before moving on to the next one. This is intuitive and works well on a single threaded machine. However, this is not how modern GPUs work at all. The new architecture kind of imitates how GPUs process triangles.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <algorithm>
 #include <cmath>
+#include <cassert>
 #include "SDL2/SDL.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
@@ -13,6 +14,12 @@ const TGAColor blue = TGAColor(0, 0, 255, 255);
 #define WINDOW_HEIGHT 1024
 #define WINDOW_WIDTH 1024
 Vector3f lightDirection(0, 0, -1);
+
+#ifndef NDEBUG
+  bool isOrthogonal(Vector3f A, Vector3f B){
+    return fabs(A.dot(B)) < 0.00001;
+  }
+#endif
 
 class boundingBox{
  private:
@@ -42,13 +49,13 @@ class boundingBox{
   //     bboxBottomRight.x,bboxBottomRight.y, renderer, color);
 };
 
-class zbuffer{
+class ZBuffer{
  private:
   int screenWidth;
   int screenHeight;
   std::vector<std::vector<float>> buffer;
  public:
-  zbuffer(int screenWidth_, int screenHeight_)
+  ZBuffer(int screenWidth_, int screenHeight_)
       : screenWidth(screenWidth_),
         screenHeight(screenHeight_),
         buffer(screenWidth_, std::vector<float>(screenHeight_,
@@ -106,20 +113,84 @@ Vector3f barycentricCoords(Vector2i A, Vector2i B, Vector2i C, Vector2i P)
   return Vector3f(1-u(0)-u(1), u(0), u(1));
 }
 
+
 class triangle{
  private:
+  //Vertices
   std::array<Vector3f,3> v;
+  //Texture coordinates
+  std::array<Vector2f,3> uv;
+  //Surface normal
+  Vector3f normal;
+  std::vector<Vector2f> fragments;
+  Vector3f calculateNormal(){
+    return (v[2]-v[0]).cross((v[1]-v[0]));
+  } 
  public:
-  triangle(Vector3f v0_, Vector3f v1_, Vector3f v2_) {
-    v[0] = v0_;
-    v[1] = v1_;
-    v[2] = v2_;
+  Vector3f calculateNormal2(){
+    return (v[1]-v[0]).cross((v[2]-v[1]));
+  } 
+  triangle() {}
+  triangle(Vector3f v0, Vector3f v1, Vector3f v2) {
+    v[0] = v0;
+    v[1] = v1;
+    v[2] = v2;
+    normal = calculateNormal();     
+  }
+  triangle(Vector3f v0, Vector3f v1, Vector3f v2,
+           Vector2f uv0, Vector2f uv1, Vector2f uv2) {
+    v[0] = v0;
+    v[1] = v1;
+    v[2] = v2;
+    uv[0] = uv0;
+    uv[1] = uv1;
+    uv[2] = uv2;
+    normal = calculateNormal();      
   }
 
-  triangle(std::array<Vector3f, 3> vertices) {
-    v[0] = vertices[0];
-    v[1] = vertices[1];
-    v[2] = vertices[2];
+  triangle(std::array<Vector3f, 3> v_, std::array<Vector2f, 3> uv_) {
+    v[0] = v_[0];
+    v[1] = v_[1];
+    v[2] = v_[2];
+    uv[0] = uv_[0];
+    uv[1] = uv_[1];
+    uv[2] = uv_[2];
+    normal = calculateNormal();      
+  }
+
+  std::array<Vector3f,3> getVertices(){
+    return v;
+  }
+
+  std::array<Vector2f,3> getUv(){
+    return uv;
+  }
+
+  void createFragment(Vector2f coords){
+    fragments.push_back(coords);
+  }
+
+  std::vector<Vector2f>& getFragments(){
+    return fragments;
+  }
+
+  void transform(Matrix4f M, Matrix4f N, Matrix4f viewport = Matrix4f::Identity()){ 
+    Vector4f temp;
+    for (int i = 0; i < 3; i++)
+    {
+      temp = Vector4f(v[i](0), v[i](1), v[i](2), 1.0f);
+      temp = M*temp;
+      v[i] = Vector3f(temp(0)/temp(3),temp(1)/temp(3),temp(2)/temp(3));
+    }
+    temp = Vector4f(normal(0),normal(1),normal(2),0);
+    temp = N*temp;
+    normal = Vector3f(temp(0),temp(1),temp(2));
+    for (int i = 0; i < 3; i++)
+    {
+      temp = Vector4f(v[i](0), v[i](1), v[i](2), 1.0f);
+      temp = viewport*temp;
+      v[i] = Vector3f(temp(0)/temp(3),temp(1)/temp(3),temp(2)/temp(3));
+    }
   }
 
   bool isInsideTriangle(Vector2i P)
@@ -136,9 +207,13 @@ class triangle{
     return u(0) / u(2) >= 0. && u(1) / u(2) >= 0. && (u(0) / u(2) + u(1) / u(2)) <= 1.;
   }
 
+  Vector3f getNormal(){
+    return normal;
+  }
+
   void draw(SDL_Renderer *renderer,
             std::array<Vector2f,3> uv, TGAImage &texture,
-            zbuffer &zBuffer, Matrix4f &projection,
+            ZBuffer &zBuffer, Matrix4f &projection,
             Matrix4f &view){
 
     // Matrix4f projection = orthographic(-1,1,-1,1,1,-1);
@@ -196,17 +271,15 @@ class triangle{
           {
             //determine texture coordinates
             Vector3f barycentricCoords_ = barycentricCoords(Vector2i(v[0](0),v[0](1)), Vector2i(v[1](0),v[1](1)), Vector2i(v[2](0),v[2](1)), Vector2i(P(0),P(1)));
-            Vector2f interpolatedUv;
-            for (int i = 0; i < 3; i++)
-            {
-              interpolatedUv(0) = uv[0](0) * barycentricCoords_(0)
-                              + uv[1](0) * barycentricCoords_(1)
-                              + uv[2](0) * barycentricCoords_(2);
 
-              interpolatedUv(1) = uv[0](1) * barycentricCoords_(0)
-                              + uv[1](1) * barycentricCoords_(1)
-                              + uv[2](1) * barycentricCoords_(2);
-            }
+            Vector2f interpolatedUv;
+            interpolatedUv(0) = uv[0](0) * barycentricCoords_(0)
+                            + uv[1](0) * barycentricCoords_(1)
+                            + uv[2](0) * barycentricCoords_(2);
+
+            interpolatedUv(1) = uv[0](1) * barycentricCoords_(0)
+                            + uv[1](1) * barycentricCoords_(1)
+                            + uv[2](1) * barycentricCoords_(2);
 
             TGAColor color = texture.get(interpolatedUv(0) * texture.width(),
                                         interpolatedUv(1) * texture.height());
@@ -217,6 +290,118 @@ class triangle{
             zBuffer.set(P(0),P(1),P(2));
             //std::cout << P(2) << "\n";
           }
+        }
+      }
+    }
+  }
+};
+
+class VertexShader{
+ private:
+  Vector3f lookDir;
+  bool isBackface(triangle t){
+    Vector3f normalVector = t.getNormal();
+    if(normalVector.dot(lookDir) < 0)
+      return true;
+    return false;
+  }
+
+ public:
+  VertexShader() : lookDir(Vector3f(0,0,-1.0)) {}
+
+  void shade(std::vector<triangle> &triangles,
+             Matrix4f MVP,
+             Matrix4f viewport,
+             bool doBackfaceCulling = 1){
+    Matrix4f N = (MVP.inverse()).transpose();
+    if(doBackfaceCulling){
+      for (auto it=triangles.begin(); it!=triangles.end();)
+      {
+        it->transform(MVP,N,viewport);
+        if(isBackface(*it))
+          it=triangles.erase(it);
+        else
+          it++;
+      }
+    }
+    else{
+      //to be implemented, not really needed though
+    }
+  }
+};
+
+class Rasterizer{
+ public:
+  void rasterize(std::vector<triangle> &triangles){
+    for (int i = 0; i < triangles.size(); i++){
+      std::array<Vector3f,3> v = triangles[i].getVertices();
+      boundingBox bbox(Vector2f(WINDOW_WIDTH - 1, WINDOW_HEIGHT - 1), Vector2f(0, 0));
+      for (int i = 0; i < 3; i++)
+      {
+        bbox.TR()(0) = std::max(bbox.TR()(0), v[i](0));
+        bbox.TR()(1) = std::max(bbox.TR()(1), v[i](1));
+
+        bbox.BL()(0) = std::min(bbox.BL()(0), v[i](0));
+        bbox.BL()(1) = std::min(bbox.BL()(1), v[i](1));
+      }
+
+      Vector3f P;
+      for (P(0) = bbox.BL()(0); P(0) <= bbox.TR()(0); P(0)++){
+        for (P(1) = bbox.BL()(1); P(1) <= bbox.TR()(1); P(1)++){
+          if(!(P(0) >=0 && P(0) <WINDOW_WIDTH && P(1) >= 0 && P(1) <WINDOW_HEIGHT))
+            continue;
+          if (triangles[i].isInsideTriangle(Vector2i(P(0), P(1)))){
+            triangles[i].createFragment(Vector2f(P(0), P(1)));
+          }
+        }
+      }
+    }
+  }
+};
+
+class FragmentShader{
+ public:
+  void shade(std::vector<triangle> &triangles, ZBuffer &zbuffer, TGAImage &texture,
+             SDL_Renderer *renderer, Vector3f lightDir){
+    for (int i = 0; i < triangles.size(); i++)
+    {
+      Vector3f normal = triangles[i].getNormal();
+      normal.normalize();
+      float intensity = normal.dot(lightDir);
+      std::vector<Vector2f> fragments = triangles[i].getFragments();
+      std::array<Vector3f,3> v = triangles[i].getVertices();
+      std::array<Vector2f,3> uv = triangles[i].getUv();
+      //something's fishy about the normal calculation
+      normal = triangles[i].calculateNormal2();
+      normal.normalize();
+      for (int j = 0; j < fragments.size(); j++)
+      {
+        Vector3f P(fragments[j](0), fragments[j](1), 0);
+        float A = normal(0) * (v[0](0) - P(0));
+        float B = normal(1) * (v[0](1) - P(1));
+        P(2) = A + B / normal(2) + v[0](2);
+        if (P(2) > zbuffer.get(P(0),P(1)))
+        {
+          //determine texture coordinates
+          Vector3f barycentricCoords_ = barycentricCoords(Vector2i(v[0](0),v[0](1)), Vector2i(v[1](0),v[1](1)), Vector2i(v[2](0),v[2](1)), Vector2i(P(0),P(1)));
+
+          Vector2f interpolatedUv;
+          interpolatedUv(0) = uv[0](0) * barycentricCoords_(0)
+                          + uv[1](0) * barycentricCoords_(1)
+                          + uv[2](0) * barycentricCoords_(2);
+
+          interpolatedUv(1) = uv[0](1) * barycentricCoords_(0)
+                          + uv[1](1) * barycentricCoords_(1)
+                          + uv[2](1) * barycentricCoords_(2);
+
+          TGAColor color = texture.get(interpolatedUv(0) * texture.width(),
+                                      interpolatedUv(1) * texture.height());
+
+          SDL_SetRenderDrawColor(renderer, intensity*color[2],
+                                          intensity*color[1],
+                                          intensity*color[0], 255);
+          SDL_RenderDrawPoint(renderer, P(0), WINDOW_HEIGHT-P(1)-1);
+          zbuffer.set(P(0),P(1),P(2));
         }
       }
     }
@@ -346,13 +531,26 @@ int main(int argc, char **argv)
   SDL_RenderClear(renderer);
   SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-  zbuffer zBuffer(WINDOW_WIDTH,WINDOW_HEIGHT);
-
+  ZBuffer zBuffer(WINDOW_WIDTH,WINDOW_HEIGHT);
+  VertexShader vertShader;
+  Rasterizer rasterizer;
+  FragmentShader fragShader;
   TGAImage texture;
-  texture.read_tga_file("obj/african_head_diffuse.tga");
+  texture.read_tga_file("/home/yavuz/Desktop/Software-Renderer/obj/african_head_diffuse.tga");
   texture.flip_vertically();
+  std::vector<triangle> rawTriangles;
+  for (int i = 0; i < model->nfaces(); i++)
+  {
+    std::vector<int> face = model->face(i);
+    std::vector<int> textureIdx = model->textureIdx(i);
+    rawTriangles.emplace_back(triangle(model->vert(face[0]),
+                                       model->vert(face[1]),
+                                       model->vert(face[2]),
+                                       model->uv(textureIdx[0]),
+                                       model->uv(textureIdx[1]),
+                                       model->uv(textureIdx[2])));
+  }
 
-  std::array<Vector2f,3> uv;
   float x,y;
   int angle;
   float r = 4.0;
@@ -364,54 +562,26 @@ int main(int argc, char **argv)
     angle+=delta%360;
     Matrix4f projection = orthographic(-1,1,-1,1,1,-1);
     Matrix4f view = lookAt(Vector3f(3*x,0,3*y),Vector3f(0,0,0),Vector3f(0,1,0));
+    Matrix4f vp = viewport(WINDOW_HEIGHT, WINDOW_WIDTH);
+    Matrix4f M = projection*view;
+    std::vector<triangle> triangles(rawTriangles);
     SDL_SetRenderDrawColor(renderer,0,0,0,255);
     SDL_RenderClear(renderer);
     zBuffer.resetBuffer();
-    for (int i = 0; i < model->nfaces(); i++)
-    {
-      std::vector<int> face = model->face(i);
-      std::vector<int> textureIdx = model->textureIdx(i);
-      std::array<Vector2f,3> uv;
-      for (int j = 0; j < 3; j++)
-      {
-        uv[j] = model->uv(textureIdx[j]);
-      }
-      triangle t(model->vert(face[0]), model->vert(face[1]), model->vert(face[2]));\
-      t.draw(renderer, uv, texture, zBuffer, projection, view);
+    vertShader.shade(triangles,M,vp);
+    rasterizer.rasterize(triangles);
+    fragShader.shade(triangles,zBuffer,texture,renderer,Vector3f(0,0,-1.0));
+    //zBuffer.visualize(renderer,WINDOW_WIDTH,WINDOW_HEIGHT);
+    // zBuffer.printBuffer();
+    SDL_RenderPresent(renderer);
+    if (SDL_PollEvent(&event)){
+      if(event.type == SDL_QUIT || (event.type==SDL_WINDOWEVENT && event.window.
+      event==SDL_WINDOWEVENT_CLOSE))
+        break;
     }
-      // zBuffer.visualize(renderer,WINDOW_WIDTH,WINDOW_HEIGHT);
-      // zBuffer.printBuffer();
-      SDL_RenderPresent(renderer);
-      if (SDL_PollEvent(&event)){
-        if(event.type == SDL_QUIT || (event.type==SDL_WINDOWEVENT && event.window.event==SDL_WINDOWEVENT_CLOSE))
-          break;
-      }
   }
 
-  // for (int i = 0; i < model->nfaces(); i++)
-  // {
-  //   std::vector<int> face = model->face(i);
-  //   std::vector<int> textureIdx = model->textureIdx(i);
-  //   std::array<Vector2f,3> uv;
-  //   for (int j = 0; j < 3; j++)
-  //   {
-  //     uv[j] = model->uv(textureIdx[j]);
-  //   }
-
-  //   interpolatedTriangle(std::array<Vector3f, 3>{model->vert(face[0]),
-  //                                 model->vert(face[1]),
-  //                                 model->vert(face[2])},
-  //                                 renderer, uv, texture,
-  //                                 zbuffer);
-  // }
-
-  //SDL_RenderPresent(renderer);
   std::cout << "Finished rendering\n";
-  while (1)
-  {
-    if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
-      break;
-  }
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
